1d0905838260f3661adabc548d7dcf9142df90ee
wasimmahamod <wasimmahamodraza@gmail.com>
2025-01-22 21:43:33 +0600

    server and client created

---
+++ client/src/App.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handlesubmit = () => {
+    axios
+      .post("http://localhost:3000/create", {
+        name: task,
+      })
+      .then((data) => {
+        console.log(data);
+        alert("data send successfull");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getTodos = () => {
+    axios.get("http://localhost:3000/alltodos").then((data) => {
+      setTodos(data.data);
+    });
+  };
+  useEffect(() => {
+    getTodos();
+  }, [todos]);
+
+  const handledelete = (id) => {
+    axios.delete(`http://localhost:3000/deletetodo/${id}`).then((data) => {
+      console.log(data);
+      alert("data deleted successfull");
+    });
+  };
+
+  return (
+    <div className=" flex flex-col justify-center items-center ">
+      <input
+        onChange={(e) => setTask(e.target.value)}
+        className=" border border-gray-500 px-4 "
+        type="text"
+      />
+      <button
+        onClick={handlesubmit}
+        className=" bg-green-500 text-white py-1 px-3 rounded-md"
+      >
+        Submit
+      </button>
+      <div>
+        <ul>
+          {todos.map((item) => {
+            return (
+              <li className=" mt-3">
+                {item.name}{" "}
+                <button
+                  onClick={() => handledelete(item._id)}
+                  className=" bg-red-500 text-white py-1 px-2 rounded-md"
+                >
+                  delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+//connect database
+mongoose
+  .connect(
+    "mongodb+srv://crud:crud@cluster0.qa3ft.mongodb.net/crud?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("database is connected ");
+  });
+
+//   model
+const todo = mongoose.model("Todo", {
+  name: String,
+  age: Number,
+  email: String,
+});
+
+//  add task
+app.post("/create", (req, res) => {
+  console.log(req.body.name);
+
+  const result = new todo({
+    name: req.body.name,
+  });
+
+  result.save();
+  res.status(201).send({ success: true, msg: "todo created successfull" });
+});
+// all todos get
+app.get("/alltodos", async (req, res) => {
+  let alltodos = await todo.find({});
+  res.send(alltodos);
+});
+
+// delete todo
+
+app.delete("/deletetodo/:id", async (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let deletetodo = await todo.findOneAndDelete({
+    _id: id,
+  });
+  res
+    .status(200)
+    .send({ success: true, msg: "deleted successfull", data: deletetodo });
+});
+
+app.listen(3000, () => {
+  console.log("server is running ");
+});
